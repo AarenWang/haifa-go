0279ebb06328ca899a2875e66c33821d243be5ed
AarenWang <AarenWang@github.com>
2023-01-08 14:45:39 +0800

    etcd lease

--- etcd/client_test.go
+++ etcd/client_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"go.etcd.io/etcd/clientv3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"io/ioutil"
 	"log"
 	"os"
@@ -15,8 +15,64 @@ import (
 var (
 	dialTimeout    = 5 * time.Second
 	requestTimeout = 4 * time.Second
-	endpoints      = []string{"192.168.31.17:2379"}
+	//endpoints      = []string{"192.168.31.17:2379"}
+	endpoints = []string{"127.0.0.1:2379"}
 )
+
+func TestLease(t *testing.T) {
+
+	cfg := clientv3.Config{
+		Endpoints: endpoints,
+	}
+
+	cli, err := clientv3.New(cfg)
+
+	if err != nil {
+		//log.Fatal(err)
+		panic(err)
+	}
+
+	defer cli.Close()
+
+	// 创建一个3秒的租约
+	lease := clientv3.NewLease(cli)
+	grantResp, err := lease.Grant(context.Background(), 3) // 3秒后过期
+	if err != nil {
+		//log.Fatal(err)
+		panic(err)
+	}
+	leaseID := grantResp.ID
+
+	// 5秒后过期的key
+	key := "testkey"
+	value := "testvalue"
+
+	// 设置key的过期时间
+	_, err = cli.Put(context.Background(), key, value, clientv3.WithLease(clientv3.LeaseID(leaseID)))
+	if err != nil {
+		//log.Fatal(err)
+		panic(err)
+	}
+
+	// 保持租约
+	lease.TimeToLive(context.Background(), clientv3.LeaseID(leaseID), clientv3.WithAttachedKeys())
+
+	// 等等超过3秒
+	time.Sleep(4 * time.Second)
+
+	// 5秒后过期的key
+	rsp, err := cli.Get(context.Background(), key)
+	if err != nil {
+		//log.Println("Get failed. ", err)
+		panic(err)
+	} else {
+		for i, kv := range rsp.Kvs {
+			log.Printf("Get index %d , key=%s, value= %s\n", i, kv.Key, kv.Value)
+		}
+	}
+
+	log.Println("Done!")
+}
 
 func TestTLSClient(t *testing.T) {
 	var homedir = os.Getenv("HOME")
